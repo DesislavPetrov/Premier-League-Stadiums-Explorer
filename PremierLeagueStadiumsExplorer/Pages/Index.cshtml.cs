d0f9c0ba4dc4fed7c70f98e3022c998b21a568eb
unknown <petrov_desislav@abv.bg>
2018-10-29 22:34:14 +0200

    added stadiums to themap, but not fully  working

--- PremierLeagueStadiumsExplorer/Pages/Index.cshtml.cs
+++ PremierLeagueStadiumsExplorer/Pages/Index.cshtml.cs
@@ -1,14 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using GeoJSON.Net.Feature;
+using GeoJSON.Net.Geometry;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.IO;
 
 namespace PremierLeagueStadiumsExplorer.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
         public string MapboxAccessToken { get; }
 
-        public IndexModel(IConfiguration configuration)
+        public IndexModel(IConfiguration configuration, IHostingEnvironment  hostingEnvironment)
         {
+            _hostingEnvironment = hostingEnvironment;
             MapboxAccessToken = configuration["Mapbox:AccessToken"];
         }
 
@@ -16,5 +26,39 @@ namespace PremierLeagueStadiumsExplorer.Pages
         {
             
         }
+
+        public IActionResult OnGetStadiums()
+        {
+            var configuration = new Configuration
+            {
+                BadDataFound = context =>
+                {
+
+                }
+            };
+
+            using (var sr = new StreamReader(Path.Combine(_hostingEnvironment.WebRootPath, "stadiums.dat")))
+            using (var reader = new CsvReader(sr, configuration))
+            {
+                FeatureCollection featureCollection = new FeatureCollection();
+                while (reader.Read())
+                {
+                    string name = reader.GetField<string>(1);
+                    string city = reader.GetField<string>(2);
+                    double latitude = reader.GetField<double>(3);
+                    double longitude = reader.GetField<double>(4);
+
+                    featureCollection.Features.Add(new Feature(
+                        new Point(new Position(latitude, longitude)),
+                        new Dictionary<string, string>
+                        {
+                            { "name", name},
+                            { "city", city}
+                        }));
+                }
+                return new JsonResult(featureCollection);
+            }            
+        }
     }
 }
+
